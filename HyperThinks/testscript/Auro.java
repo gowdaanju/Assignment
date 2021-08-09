f84a71d414d10143aff2c4203cffef425640fa64
gowdaanju <74400478+gowdaanju@users.noreply.github.com>
2021-08-09 08:26:20 +0530

    Update Auro.java

--- HyperThinks/testscript/Auro.java
+++ HyperThinks/testscript/Auro.java
@@ -11,29 +11,18 @@ import com.aventstack.extentreports.Status;
 
 import generic.ExcelSheet;
 import generic.Precondition;
-import pom.Auro_Categories_Page;
-import pom.Auro_Checkout_Page;
-import pom.Auro_Goals_Page;
-import pom.Auro_Home_Page;
-import pom.Auro_Level_Page;
-import pom.Auro_SignUp_Page;
 import pom.Login_Page;
 
 
-public class Auro extends Precondition {
+public class Login extends Precondition {
 	
 	@Test(invocationCount =1)
 	public void hyperThinkLoginVerification() throws InterruptedException, IOException {
-		
-//		test=extent.createTest("HyperThink Login Page");
-//		test.log(Status.INFO, "HyperThink login Test Script Started Executed");
 			
 		String username = ExcelSheet.readDataFromexcel("Sheet1", 1, 0);
 		String password = ExcelSheet.readDataFromexcel("Sheet1", 1, 1);
 		System.out.println("username is "+username);
 		System.out.println("password is "+password);
-		
-		
 		
 		Login_Page lp = new Login_Page(driver);
 		
@@ -45,6 +34,7 @@ public class Auro extends Precondition {
 		//softAssert.assertEquals(actuaURL, "https://hyperthings.om/login/home");
 		//test.pass("URL verified");	
 		ExcelSheet.writeDataToExcel("Sheet1", 1, 2, "Failed");
+		//softAssert.assertAll();
 		
 				
 	}
